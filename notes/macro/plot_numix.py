8ea67bbd11da06389175b0138d04fb89455c7221
Luca Baldini <luca.baldini@pi.infn.it>
2026-03-18 05:55:34 +0100

    Neutrino oscillations in matter completed.

--- notes/macro/plot_numix.py
+++ notes/macro/plot_numix.py
@@ -73,20 +73,23 @@ setup_ca(xlabel='$L$ [km]', ylabel='Oscillation probability', ymax=1., grids=Fal
 save_cf()
 
 
-margin_figure('msw conversion', height=2.5, left=0.2)
-zeta = np.linspace(0., 2.5, 100)
-plt.hlines(numix.DELTA_M21_CBE, 0., 1., color='black')
-plt.hlines(numix.DELTA_M21_CBE, 1., 2.5, color='black', ls='dashed')
-mask = zeta < 1
-plt.plot(zeta[mask], zeta[mask] * numix.DELTA_M21_CBE, color='black', ls='dashed')
-mask = zeta >= 1
-plt.plot(zeta[mask], zeta[mask] * numix.DELTA_M21_CBE, color='black')
-plt.text(2., numix.DELTA_M21_CBE, '$\\nu_\\mu$', va='bottom')
-plt.text(2., 2.3 * numix.DELTA_M21_CBE, '$\\nu_e$', va='bottom')
-plt.text(0.1, 2.4 * numix.DELTA_M21_CBE, '$\\rightarrow$ increasing $\\varrho$')
+margin_figure('msw conversion', height=2.5, left=0.225)
+delta_m2 = numix.DELTA_M21_CBE
+m1sq = 0
+m2sq = delta_m2
+zeta = np.linspace(0., 4., 100)
+plt.axhline(numix.DELTA_M21_CBE, color='black')
+plt.plot(zeta, zeta * numix.DELTA_M21_CBE, color='black')
+plt.text(2., 1.1 * numix.DELTA_M21_CBE, '$\\nu_\\mu = \\nu_2$', va='bottom')
+plt.text(2.6, 2.3 * numix.DELTA_M21_CBE, '$\\nu_e = \\nu_1$', va='bottom')
+plt.text(0.1, 3.5 * numix.DELTA_M21_CBE, '$\\rightarrow$ increasing $\\varrho$')
 plt.plot(1., numix.DELTA_M21_CBE, 'o', color='black', ms=4)
-plt.text(1., 0.95 * numix.DELTA_M21_CBE, 'Resonant conversion', ha='left', va='top', size='x-small')
-setup_ca(xlabel='$\\zeta$', ylabel='$m^2$', yticks=(), xticks=(0., 1., 2.))
+plt.text(1., 0.9 * numix.DELTA_M21_CBE, 'Resonant conversion',
+         ha='left', va='top', size='x-small')
+setup_ca(xlabel='$\\zeta$', ylabel='$\\tilde{m}^2_i$', yticks=(m1sq, m2sq), ymin=0.,
+         xticks=(0., 1., 2., 3., 4.))
+plt.gca().yaxis.set_label_coords(-0.12, 0.5)
+plt.gca().set_yticklabels(('$m_1^2$', '$m_2^2$'))
 save_cf()
 
 margin_figure('msw resonance energy', height=2.5)
@@ -100,13 +103,39 @@ setup_ca(xlabel='$r$ [$R_\\odot$]', ylabel='$E_c$ [MeV]', xticks=(0., 0.1, 0.2),
 save_cf()
 
 
-margin_figure('msw mixing', height=2.5)
-zeta = np.linspace(0., 5, 100)
+margin_figure('msw mixing', height=2.5, left=0.275)
+zeta = np.linspace(0., 4, 100)
 c2t = np.cos(2. * np.radians(numix.THETA_12_CBE))
 s2t = np.sin(2. * np.radians(numix.THETA_12_CBE))
 C = np.sqrt((c2t - zeta)**2. + (s2t)**2.)
 theta = 0.5 * np.atan2(s2t, c2t - zeta)
 plt.plot(zeta, np.degrees(theta), color='black')
+plt.axhline(numix.THETA_12_CBE, color='black', ls='dashed')
+plt.text(1.5, numix.THETA_12_CBE + 1., '$\\theta$ in vacuum', ha='left', va='bottom')
+setup_ca(xlabel='$\\zeta$', ylabel='$\\tilde{\\theta}$', ymin=0., ymax=90.,
+         xticks=(0., 1., 2., 3., 4.))
+save_cf()
+
+
+margin_figure('msw mass', height=2.5, left=0.225)
+zeta = np.linspace(0., 4, 100)
+C = np.sqrt((c2t - zeta)**2. + (s2t)**2.)
+delta_m2 = numix.DELTA_M21_CBE
+m1sq = 0
+m2sq = delta_m2
+m1_tilde2 = 0.5 * (m1sq + m2sq + zeta * delta_m2 - C * delta_m2)
+m2_tilde2 = 0.5 * (m1sq + m2sq + zeta * delta_m2 + C * delta_m2)
+plt.plot(zeta, m1_tilde2, color='black')
+plt.plot(zeta, m2_tilde2, color='black')
+plt.text(0.1, 3.75 * numix.DELTA_M21_CBE, '$\\rightarrow$ increasing $\\varrho$')
+plt.text(2., m2sq - 2.e-5, "$\\nu_1$")
+plt.text(2.5, m2sq + 1.2e-4, "$\\nu_2$")
+setup_ca(xlabel='$\\zeta$', ylabel='$\\tilde{m}^2_i$', yticks=(m1sq, m2sq),
+         ymin=0., xticks=(0., 1., 2., 3., 4.))
+plt.gca().yaxis.set_label_coords(-0.12, 0.5)
+plt.gca().set_yticklabels(('$m_1^2$', '$m_2^2$'))
+save_cf()
+
 
 print(numix.DELTA_M21_CBE / 2. / np.sqrt(2.) / numix.G_F / 1.e6)
 print(numix.msw_resonance_energy(BS05.electron_numer_density(0.)))
